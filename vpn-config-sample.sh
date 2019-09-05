d1480611a317fb166f0ae79a39d7364447744a49
Stephan Müller <smueller@suse.com>
2019-09-05 08:56:39 +0200

    Added more descriptions to the config sample
    Signed-off-by: Stephan Müller <smueller@suse.com>

--- vpn-config-sample.sh
+++ vpn-config-sample.sh
@@ -1,20 +1,23 @@
 # You can see those with "nmcli con show --active" command
+# The first UID will be prefered
 VPN_UIDS=("FIRST-VPN-UID-HERE" "SECOND-VPN-UID-HERE")
 VPN_PW_FILE="path/to/secrect"
 DEVICE="network device name"
- 
+
 # Delay in secconds
 DELAY=60
-PING_TIMEOUT=5
- 
+PING_TIMEOUT=10
+
 # File path with write permission to the executing user to store script status information
 LOG="/path/to/log/file.log"
- 
+
 # Enable/disable ping connection check
+# If all set hostnames timeout on a ping the next vpn will be tried or
+# the connection will be reseted if only one vpn is set.
 PING_CHECK_ENABLED=true
- 
+
 # Check IPs/Hostnames
 HOSTS=("8.8.8.8" "host.name")
- 
+
 # Configure DISPLAY variable for desktop notifications
 DISPLAY=0.0
